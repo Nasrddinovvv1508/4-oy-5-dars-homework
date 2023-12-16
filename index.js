feb8e1a35cd4decaa1b4114452a12c9fcfde468a
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-16 20:39:25 +0500

    All done

---
+++ index.js
@@ -0,0 +1,44 @@
+    // 1-masala
+// let a = +prompt('Birinchi sonni kiriting...');
+// let b = +prompt('Ikkinchi sonni kiriting...');
+// let res = 1;
+// let i = a;
+
+// while (i <= b) {
+//     res *= i;
+//     i++;
+// }
+
+// console.log(res);
+
+
+
+    // 2-masala
+// let n = +prompt('Sonni kiriting...');
+// let counter = 0;
+
+// while (n >= 1) {
+//     counter++;
+//     n = Math.trunc(n / 10);
+// }
+
+// console.log(counter);
+
+
+
+    // 3-masala
+// let y = +prompt('Sonni kiriting...');
+// let i = 1;
+// let res = 1;
+
+// while (y >= i) {
+//     res *= i;
+//     i++;
+// }
+
+// console.log(res);
+
+
+
+
+    // break((
